6c7f3efc41edc9c98a3f82d630f13f74aa5a6ef4
Sunchy321 <sunchy321@qq.com>
2024-04-19 16:15:12 +0800

    update 0419

--- src/xc_ast/src/token.rs
+++ src/xc_ast/src/token.rs
@@ -4,10 +4,112 @@ pub enum CommentKind {
 }
 
 pub enum Delimiter {
+    /// `(` `)`
     Paren,
+    /// `[` `]`
     Bracket,
+    /// `{` `}`
     Brace,
+    /// `{|` `|}`
     DictBound,
+}
+
+pub enum OpKind {
+    /// `+!`
+    Successor,
+    /// `-!`
+    Predecessor,
+    /// `?`
+    Null,
+
+    /// `!`
+    Not,
+    /// `'~`
+    BitNot,
+
+    /// `*`
+    Multiply,
+    /// `/`
+    Divide,
+    /// `%`
+    Modulo,
+
+    /// `+`
+    Plus,
+    /// `-`
+    Subtract,
+
+    /// `'&`
+    BitAnd,
+    /// `'^`
+    BitXor,
+    /// `'|`
+    BitOr,
+
+    /// `..`
+    Range,
+    /// `..=`
+    ClosedRange,
+
+    /// `~`
+    Concat,
+
+    /// `??`
+    NullCoalescing,
+
+    /// `==`
+    Equal,
+    /// `!=`
+    NotEqual,
+    /// `<`
+    Less,
+    /// `>`
+    Greater,
+    /// `<=`
+    LessEqual,
+    /// `>=`
+    GreaterEqual,
+    /// `<>`
+    LessGreater,
+
+    /// `&`
+    And,
+    /// `|`
+    Or,
+
+    /// `=`
+    Assign,
+    /// `+=`
+    PlusAssign,
+    /// `-=`
+    SubtractAssign,
+    /// `*=`
+    MultiplyAssign,
+    /// `/=`
+    DivideAssign,
+    /// `%=`
+    ModuloAssign,
+    /// `'&=`
+    BitAndAssign,
+    /// `'^=`
+    BitXorAssign,
+    /// `'|=`
+    BitOrAssign,
+    /// `??=`
+    NullCoalescingAssign,
+    /// `++`
+    Increment,
+    /// `--`
+    Decrement,
+    /// `~>`
+    ConcatLeft,
+    /// `<~`
+    ConcatRight,
+
+    /// `;`
+    Semicolon,
+
+    Custom(&str),
 }
 
 pub enum LiteralKind {
@@ -16,4 +118,36 @@ pub enum LiteralKind {
     Floating,
     String,
     Symbol,
+}
+
+pub struct Literal {
+    pub kind: LiteralKind,
+    pub value: String,
+    pub suffix: Option<String>,
+}
+
+pub enum TokenKind {
+    /// `:`
+    Colon,
+    /// `::`
+    ColonColon,
+    /// `,`
+    Comma,
+    /// `@`
+    At,
+
+    Op(OpKind),
+
+    OpenDelim(Delimiter),
+
+    ClosedDelim(Delimiter),
+}
+
+pub struct Span {
+
+}
+
+pub struct Token {
+    pub kind: TokenKind,
+    pub span: Span,
 }
