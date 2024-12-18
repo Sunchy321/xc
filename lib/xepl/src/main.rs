8fc5e61524694f76e8f7415c1bc9f1af40f9d469
Sunchy321 <sunchy321@qq.com>
2024-12-18 17:19:12 +0800

    feat: add color for repl

--- lib/xepl/src/main.rs
+++ lib/xepl/src/main.rs
@@ -3,24 +3,40 @@ mod store;
 mod r#type;
 mod value;
 
-use std::io::{stdin, stdout, Write};
+use std::io::{stdin, Write};
 
 use eval::eval;
 use store::Store;
+use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
+use value::println_value;
 use xc_parse::parser_from_source_str;
 use xc_parse::session::ParseSession;
 use xc_span::source_map::Filename;
 use xc_span::create_session_globals_then;
 
+fn with_fg(color: Color) -> ColorSpec {
+    let mut spec = ColorSpec::new();
+
+    spec.set_fg(Some(color));
+
+    spec
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let hint = with_fg(Color::Ansi256(8));
+    let error = with_fg(Color::Red);
+
     let session = ParseSession::new();
 
     let store = Store::new();
 
-    create_session_globals_then::<Result<(), Box<dyn std::error::Error>>>(|| loop {
-        print!("> ");
+    let mut stdout = StandardStream::stdout(ColorChoice::Always);
 
-        stdout().flush()?;
+    create_session_globals_then::<Result<(), Box<dyn std::error::Error>>>(|| loop {
+        stdout.set_color(&hint)?;
+        write!(stdout, "> ")?;
+        stdout.reset()?;
+        stdout.flush()?;
 
         let mut source = String::new();
 
@@ -35,15 +51,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let mut parser = parser_from_source_str(&session, name, source);
 
         let Ok(expr) = parser.parse_expr() else {
-            println!("Error while parsing!");
+            stdout.set_color(&error)?;
+            writeln!(stdout, "Error while parsing!")?;
+            stdout.reset()?;
             continue;
         };
 
         let Ok(result) = eval(&*expr) else {
-            println!("Error while evaluating!");
+            stdout.set_color(&error)?;
+            writeln!(stdout, "Error while evaluating!")?;
+            stdout.reset()?;
             continue;
         };
 
-        println!("{}", result);
+        println_value(result, &mut stdout, &hint)?;
     })
 }
--- lib/xepl/src/value.rs
+++ lib/xepl/src/value.rs
@@ -1,5 +1,6 @@
 use std::fmt::Display;
 
+use termcolor::{ColorSpec, WriteColor};
 use xc_ast::literal::{Literal, LiteralKind};
 use xc_span::symbol::kw;
 
@@ -15,23 +16,53 @@ impl Value {
         use LiteralKind::*;
 
         match lit.kind {
+            Nil => Value::Nil,
+
             Bool => if lit.value == kw::True {
                 Value::Bool(true)
             } else {
                 Value::Bool(false)
             },
 
+
+
             _ => todo!()
         }
     }
+
+    pub fn print_value(&self, writer: &mut impl WriteColor) -> Result<(), Box<dyn std::error::Error>> {
+        match self {
+            Value::Nil => write!(writer, "nil")?,
+            Value::Bool(b) => write!(writer, "{}", b)?,
+            Value::Int(i) => write!(writer, "{}", i)?,
+        }
+
+        Ok(())
+    }
+
+    pub fn print_type(&self, writer: &mut impl WriteColor) -> Result<(), Box<dyn std::error::Error>> {
+        match self {
+            Value::Nil => write!(writer, "<unknown>?")?,
+            Value::Bool(_) => write!(writer, "bool")?,
+            Value::Int(_) => write!(writer, "int")?,
+        }
+
+        Ok(())
+    }
 }
 
-impl Display for Value {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match &self {
-            Value::Nil => write!(f, "nil"),
-            Value::Bool(b) => write!(f, "{}", b),
-            Value::Int(i) => write!(f, "{}", i),
-        }
-    }
+pub fn println_value(value: Value, writer: &mut impl WriteColor, hint: &ColorSpec) -> Result<(), Box<dyn std::error::Error>> {
+    value.print_value(writer)?;
+
+    writer.set_color(hint)?;
+
+    write!(writer, " : ")?;
+
+    value.print_type(writer)?;
+
+    writer.reset()?;
+
+    writeln!(writer)?;
+
+    Ok(())
 }
